2c70827f1bd4eedcb6ed3b7d1dada3d5035b382f
zhb <zhb2000@outlook.com>
2020-12-20 00:38:30 +0800

    Fix a bug in ProjectController.createTask Get taskId correctly now.

--- src/main/java/com/ourprojmgr/demo/controller/ProjectController.java
+++ src/main/java/com/ourprojmgr/demo/controller/ProjectController.java
@@ -213,13 +213,13 @@ public class ProjectController {
         task.setComplete(false);
         task.setCompleteAt(null);
         task.setCompleterId(null);
-        task.setId(taskJson.getId());
         task = projectService.createTask(task);
         if (taskJson.getExecutors() == null) {
             taskJson.setExecutors(new ArrayList<>());
         }
+        int taskId = task.getId();
         taskJson.getExecutors().forEach(u ->
-                projectService.addExecutor(taskJson.getId(), u.getId()));
+                projectService.addExecutor(taskId, u.getId()));
         return projectService.taskToJson(task);
     }
 
